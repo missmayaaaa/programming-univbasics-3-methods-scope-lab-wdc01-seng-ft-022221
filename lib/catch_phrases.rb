ac25a4c635d230c59dbe6790e5a52da4f2e1ac13
Maya Grant <maya.grant@utexas.edu>
2021-01-04 16:20:46 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,8 +14,10 @@ def link
 end
 
 def all_phrases
-  
+  phrase = "It's-a me, Mario!"
   puts phrase
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
+  danger = "It's Dangerous To Go Alone! Take This."
   puts danger
 end
